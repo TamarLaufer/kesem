ec41f1b575a4968cbacadf18c4be8dbcc966ff9b
Tamar Laufer <tamarlaufer@gmail.com>
2025-06-12 12:55:16 +0300

    add animation to pop up

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -7,6 +7,7 @@ import {
   PopupOverlay,
   ButtonText,
 } from "./Popup.styles";
+import { motion } from "framer-motion";
 
 type PopupPropsType = {
   text?: string;
@@ -25,14 +26,24 @@ const Popup = ({
   color,
   onClick,
 }: PopupPropsType) => {
+  const AnimatedPopup = motion(PopupContainer);
+
   return (
     <PopupOverlay>
-      <PopupContainer>
-        {icon && <div style={{ fontSize: "48px", color }}>{icon}</div>}
-        {title && <h2 style={{ color }}>{title}</h2>}
-        {text && <TextContent>{text}</TextContent>}
-        {buttonText && <ButtonText onClick={onClick}>{buttonText}</ButtonText>}
-      </PopupContainer>
+      <AnimatedPopup
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.4 }}
+      >
+        <PopupContainer>
+          {icon && <div style={{ fontSize: "48px", color }}>{icon}</div>}
+          {title && <h2 style={{ color }}>{title}</h2>}
+          {text && <TextContent>{text}</TextContent>}
+          {buttonText && (
+            <ButtonText onClick={onClick}>{buttonText}</ButtonText>
+          )}
+        </PopupContainer>
+      </AnimatedPopup>
     </PopupOverlay>
   );
 };
