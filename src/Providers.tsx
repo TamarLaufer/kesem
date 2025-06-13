ea2515fbf70397ea124f86b4ffb1363463b490cc
Tamar Laufer <tamarlaufer@gmail.com>
2025-06-13 14:05:14 +0300

    fix any

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -5,11 +5,12 @@ import { initI18n } from "@/i18n/client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/theme";
 import { useParams } from "next/navigation";
+import { i18n } from "i18next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const params = useParams();
   const lng = typeof params.lng === "string" ? params.lng : "he";
-  const [i18nInstance, setI18nInstance] = useState<any>(null);
+  const [i18nInstance, setI18nInstance] = useState<i18n | null>(null);
 
   useEffect(() => {
     initI18n(lng).then(setI18nInstance);
