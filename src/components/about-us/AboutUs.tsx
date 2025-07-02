ba080a4a4c328e97558c32cd162cad15eb6c405d
Tamar Laufer <tamarlaufer@gmail.com>
2025-07-02 19:11:24 +0300

    change text aboutUs

--- src/components/about-us/AboutUs.tsx
+++ src/components/about-us/AboutUs.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import {
   AboutUsContainer,
   AboutUsText,
@@ -17,14 +18,37 @@ const imagesList = [
 
 export default function AboutUs() {
   const text = STRINGS.ABOUT_US_PAGE.ABOUT_TEXT;
+  function renderTextWithBreaks(text: string) {
+    return text
+      .split("\n")
+      .map((line, idx) =>
+        line === "" ? (
+          <br key={idx} />
+        ) : (
+          <AboutUsText key={idx}>{line}</AboutUsText>
+        )
+      );
+  }
 
   return (
     <BackgroundWrapper>
       <AboutUsContainer>
         <TextContainer>
-          {text.split("\n").map((line, index) => (
-            <AboutUsText key={index}>{line}</AboutUsText>
-          ))}
+          {/* {paragraphs.map((paragraph, idx) => (
+            <AboutUsText key={idx}>
+              {paragraph.split("\n").map((line, lineIdx, arr) =>
+                lineIdx < arr.length - 1 ? (
+                  <React.Fragment key={lineIdx}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ) : (
+                  line
+                )
+              )}
+            </AboutUsText>
+          ))} */}
+          {renderTextWithBreaks(text)}
         </TextContainer>
         <ImagesContainer>
           {imagesList.map((oneImg) => {
